ddf92eeb16bdf949d93a1b79863e450da45a5441
MrEarv <earvinarizmendi@gmail.com>
2025-11-26 13:58:02 -0600

    Diseño prematuro de Ventas

--- ventas/ventas.php
+++ ventas/ventas.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['usuario'])) {
+    header("Location: indexLogin.php");
+    exit();
+}
+
+$rol = $_SESSION['rol'];   
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
